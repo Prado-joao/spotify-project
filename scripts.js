9c647f6efeff6c1060426f98e94168df3f4b7558
João Prado <joaosouzaprado@gmail.com>
2025-09-26 13:12:14 -0300

    Criado projeto Spotify

---
+++ scripts.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const artistsData = [
+        { name: 'Playboi Carti', image: './img/artista-carti.jpg'},
+        { name: 'The Weeknd', image: './img/artista-abel.jpg'},
+        { name: 'Djavan', image: './img/artista-djavan.jpg'},
+        { name: 'Don Toliver', image: './img/artista-dontoliver.jpg'},
+        { name: 'Iron Maiden', image: './img/artista-iron.jpg'},
+        { name: 'Exaltasamba', image: './img/artista-exalta.jpg'}
+    ];
+
+    const albumsData =[
+        { name: 'DS2', artist: 'Future',  image: './img/album-future.jpg'},
+        { name: 'DS4Ever', artist: 'Gunna' , image: './img/album-gunna.jpg'},
+        { name: 'We Dont Trust You', artist: 'Metro Boomin', image: './img/album-metro.jpg'},
+        { name: 'Dangerous', artist: 'Michael jackson', image: './img/album-michael.jpg'},
+        { name: 'Nada Como um dia após o outro', artist: 'Racionais Mcs',  image: './img/album-racionais.jpg'},
+        { name: 'Jeffery', artist: 'Young Thug', image: './img/album-thug.jpg'}
+    ];
+
+    const artistGrid = document.querySelector('.artists-grid')
+    const albumsGrid = document.querySelector('.albums-grid')
+
+    artistsData.forEach( artist=>{
+        const artistsCard = document.createElement('div')
+        artistsCard.classList.add('artists-card')
+
+        artistsCard.innerHTML = `
+            <img src="${artist.image}" alt="imagem do ${artist.name}">
+            <div>
+                <h3>${artist.name}</h3>
+                <p>artista</p>
+            </div>
+        `
+        artistGrid.appendChild(artistsCard)
+
+    })
+
+    albumsData.forEach( album=>{
+        const albumsCard = document.createElement('div')
+        albumsCard.classList.add('albums-card')
+
+        albumsCard.innerHTML = `
+            <img src="${album.image}" alt="imagem do ${album.name}">
+        <div>
+            <h3>${album.name}</h3>
+            <p>${album.artist}</p>
+        </div>
+        `
+
+        albumsGrid.appendChild(albumsCard)
+
+    
+    })
+
+    
+})
